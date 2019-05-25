6f8cffbb42ae22fd9c91027815c9184018adfdbc
mikela <mikkibba@gmail.com>
2019-05-25 18:48:31 +0200

    aaa

--- app/src/main/java/com/example/myapp/FavouriteSerieActivity.java
+++ app/src/main/java/com/example/myapp/FavouriteSerieActivity.java
@@ -1,13 +1,35 @@
 package com.example.myapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class FavouriteSerieActivity extends AppCompatActivity {
+public class FavouriteSerieActivity extends AppCompatActivity
+    {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite_serie);
+
     }
 }
+
+
+
+
+
+
+
